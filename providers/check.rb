fed43752cc2757c1e44bfff1c9ebd40e838a2fee
Dan Di Spaltro <dan@cloudkick.com>
2012-06-10 15:12:37 -0700

    Initial stab at check, copy and pasted

---
+++ providers/check.rb
@@ -0,0 +1,37 @@
+include Rackspace::CloudMonitoring
+
+action :create do
+  checks = cm.checks.new(:label => new_resource.label, :type => new_resource.type,
+                         :metadata => new_resource.metadata, :agent_id => new_resource.agent_id)
+  if @current_resource.nil? then
+    entity.save
+    new_resource.updated_by_last_action(true)
+    clear
+  else
+    # Compare attributes
+    if !entity.compare? @current_resource then
+      # It's different
+      entity.id = @current_resource.id
+      entity.save
+      new_resource.updated_by_last_action(true)
+      clear
+    else
+      new_resource.updated_by_last_action(false)
+    end
+  end
+end
+
+
+def load_current_resource
+  @current_resource = nil
+  if node[:cloud_monitoring][:entity_id] then
+    @current_resource = cm.view[node[:cloud_monitoring][:entity_id]]
+  end
+  if @current_resource == nil then
+    possible = view.select {|key, value| value.label === @new_resource.name}
+    if !possible.empty? then
+      @current_resource = possible.values.first
+      node.set[:cloud_monitoring][:entity_id] = @current_resource.identity
+    end
+  end
+end
