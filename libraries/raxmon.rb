af3cbd615e1013e0a62bcad4329ac4381734780a
Dan Di Spaltro <dan@cloudkick.com>
2012-06-10 14:14:32 -0700

    Use a databag by default

--- libraries/raxmon.rb
+++ libraries/raxmon.rb
@@ -8,8 +8,13 @@ module Rackspace
   module CloudMonitoring
 
     def cm
-      apikey = new_resource.rackspace_api_key || node[:cloud_monitoring][:rackspace_api_key]
-      username = new_resource.rackspace_username || node[:cloud_monitoring][:rackspace_username]
+      begin
+        dbag = data_bag_item("cloud_monitoring", "main")
+      rescue
+        dbag = {:rackspace_api_key => nil, :rackspace_username => nil }
+      end
+      apikey = new_resource.rackspace_api_key || dbag[:rackspace_api_key]
+      username = new_resource.rackspace_username || dbag[:rackspace_username]
       @@cm ||= Fog::Monitoring::Rackspace.new(:rackspace_api_key => apikey, :rackspace_username => username)
       @@view ||= Hash[@@cm.entities.overview.map {|x| [x.identity, x]}]
       @@cm
