e2f64127a883e097bdeb8f8a6bf2f7df22744ef6
Brandon Philips <brandon.philips@rackspace.com>
2012-10-02 16:28:07 -0700

    recipes: agent: add instead of removing the agent apt repo
    This seems to be incorrect. We want to add the agent repo.

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -6,11 +6,11 @@ cookbook_file "#{Chef::Config[:file_cache_path]}/signing-key.asc" do
 end
 
 apt_repository "cloud-monitoring" do
-  uri "http://master.packages.cloudmonitoring.rackspace.com/linux-x86_64-ubuntu-10.04"
+  uri "http://unstable.packages.cloudmonitoring.rackspace.com/linux-x86_64-ubuntu-10.04"
   distribution "cloudmonitoring"
   components ["main"]
   key "signing-key.asc"
-  action :remove
+  action :add
 end
 
 # TODO: Enable once we set it up
