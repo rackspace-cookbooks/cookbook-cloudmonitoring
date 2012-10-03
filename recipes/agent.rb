39d3619334bd22d87cb6bfe8f5c6da48593e0dd2
Brandon Philips <brandon.philips@rackspace.com>
2012-10-03 08:18:13 -0700

    recipes: agent: update the URL to use correct location
    The new URL is
    http://unstable.packages.cloudmonitoring.rackspace.com/ubuntu-10.04-x86_64

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -6,7 +6,7 @@ cookbook_file "#{Chef::Config[:file_cache_path]}/signing-key.asc" do
 end
 
 apt_repository "cloud-monitoring" do
-  uri "http://unstable.packages.cloudmonitoring.rackspace.com/linux-x86_64-ubuntu-10.04"
+  uri "http://unstable.packages.cloudmonitoring.rackspace.com/ubuntu-10.04-x86_64"
   distribution "cloudmonitoring"
   components ["main"]
   key "signing-key.asc"
