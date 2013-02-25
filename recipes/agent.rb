02851e8e2c90413f49de20bc758213e3c296a585
Phil Kates <me@philkates.com>
2013-02-25 11:43:47 -0800

    Move plugin setup to agent recipe

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -56,6 +56,20 @@ template "/etc/rackspace-monitoring-agent.cfg" do
   )
 end
 
+node['cloud_monitoring']['plugins'].each_pair do |source_cookbook, path|
+  remote_directory "cloud_monitoring_plugins_#{source_cookbook}" do
+    path node['cloud_monitoring']['plugin_path']
+    cookbook source_cookbook
+    source path
+    files_mode 0755
+    owner 'root'
+    group 'root'
+    mode 0755
+    recursive true
+    purge false
+  end
+end
+
 service "rackspace-monitoring-agent" do
   # TODO: RHEL, CentOS, ... support
   supports value_for_platform(
