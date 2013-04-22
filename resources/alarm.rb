c17958b758767cf6b2eebd8b2f24922b29d2414f
Samuel Toriel <samuel.toriel@rackspace.com>
2013-04-22 17:29:39 -0400

    Add missing resource attributes and specify notification plan id as required

--- resources/alarm.rb
+++ resources/alarm.rb
@@ -5,7 +5,7 @@ attribute :check_type, :kind_of => String
 attribute :check_id, :kind_of => String
 attribute :metadata, :kind_of => Hash
 attribute :criteria, :kind_of => String
-attribute :notification_plan_id, :kind_of => String
+attribute :notification_plan_id, :kind_of => String, :required => true
 attribute :entity_id, :kind_of => String
 attribute :entity_label, :kind_of => String
 
@@ -16,3 +16,4 @@ attribute :check_label, :kind_of => String
 
 attribute :rackspace_api_key, :kind_of => String
 attribute :rackspace_username, :kind_of => String
+attribute :rackspace_auth_url, :kind_of => String
