dc477579bf237e8ccf48aa666ac4b84428a5099d
Thomas Cate <tcate@workstation.rackspace>
2013-07-02 13:25:31 -0500

    install appropiate package on Ubuntu/Debian

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -1,15 +1,22 @@
-case node['platform']
-when "ubuntu","debian"
+cookbook_file "#{Chef::Config[:file_cache_path]}/signing-key.asc" do
+  source "signing-key.asc"
+  mode 0755
+  owner "root"
+  group "root"
+end
 
-  cookbook_file "#{Chef::Config[:file_cache_path]}/signing-key.asc" do
-    source "signing-key.asc"
-    mode 0755
-    owner "root"
-    group "root"
-  end
+
+case node['platform']
+when "ubuntu", "debian"
 
   apt_repository "cloud-monitoring" do
-    uri "http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-10.04-x86_64"
+
+    if node['platform'] == 'ubuntu'
+      uri "http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-#{node['platform_version']}-#{node['kernel']['machine']}"
+    elsif node['platform'] =='debian'
+      uri "http://stable.packages.cloudmonitoring.rackspace.com/debian-#{node['lsb']['codename']}-#{node['kernel']['machine']}"
+    end
+
     distribution "cloudmonitoring"
     components ["main"]
     key "signing-key.asc"
@@ -17,14 +24,7 @@ when "ubuntu","debian"
   end
 
 end
-# TODO: Enable once we set it up
-# apt_repository "cloud-monitoring-release" do
-#  uri "http://stable.packages.cloudmonitoring.rackspace.com/linux-x86_64-ubuntu-10.04"
-#  distribution "cloudmonitoring"
-#  components ["main"]
-#  key "signing-key.asc"
-#  action :add
-#end
+
 
 begin
   values = Chef::EncryptedDataBagItem.load('rackspace', 'cloud')
