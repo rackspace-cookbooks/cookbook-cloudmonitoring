d176b43b7a7643146a9995cc153781cba1878e9b
Thomas Cate <tcate@workstation.rackspace>
2013-08-07 12:05:07 -0500

    bump version for move to fog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "daniel.dispaltro@rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures Rackspace Cloud Monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.5"
+version          "1.0.0"
 
 depends "python"
-
+depends "xml"
