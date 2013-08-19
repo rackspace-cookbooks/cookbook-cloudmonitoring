f38931198c048a2d6f42cd5687bf8059c49ba027
Thomas Cate <tcate@workstation.rackspace>
2013-08-19 14:46:19 -0500

    move to fog 1.15.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "daniel.dispaltro@rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures Rackspace Cloud Monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends "apt", ">= 1.4.2"
 depends "python"
