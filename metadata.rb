2157a74e5f373b9517f268daffd87e15be1fdd70
Ryan Richard <ryanrichard07@gmail.com>
2013-08-23 13:23:35 -0500

    update metdata to recommend chef_gem cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email "daniel.dispaltro@rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures Rackspace Cloud Monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.0.2"
 
 depends "python"
 depends "xml"
+
+#chef_gem cookbook/library required for chef versions <= 10.12.0
+recommends "chef_gem"
