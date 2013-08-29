835cf2fcbc7c7d0ea4aa86054da78be6ded5cb36
Daniel Reichert <Daniel.Reichert@rackspace.com>
2013-08-29 11:09:43 -0700

    Removing unsupported platform_family method call
    Since we do not run windows this call does not need to be replaced.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe 'xml::ruby' unless platform_family?("windows")
-
 
 chef_gem "fog" do
   version '>= 1.15.0'
