7f23b645140f4c9576cb7b63f48078d8109ab473
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-14 11:50:14 -0600

    Metadata update

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             "rackspace-cloudmonitoring"
-maintainer       "Rackspace"
-maintainer_email "thomas.noonan@rackspace.com"
+name             "rackspace_cloudmonitoring"
+maintainer       "Rackspace, US, Inc."
+maintainer_email "rackspace-cookbooks@rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures Rackspace Cloud Monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
