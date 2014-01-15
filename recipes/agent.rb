f89146bbe826763707bc1a7b01d0f5a59df51543
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-15 17:26:55 -0600

    Refactored agent recipe to use the updated LWRP and classes

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -1,8 +1,27 @@
-case node['platform']
-when "ubuntu", "debian"
+#
+# Cookbook Name:: rackspace_cloudmonitoring
+# Recipe:: agent
+#
+# Install and configure the cloud monitoring agent on a server
+#
+# Copyright 2014, Rackspace
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+if platform_family?("debian")
   apt_repository "cloud-monitoring" do
-
+   
     if node['platform'] == 'ubuntu'
       uri "http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-#{node['platform_version']}-#{node['kernel']['machine']}"
     elsif node['platform'] =='debian'
@@ -15,8 +34,8 @@ when "ubuntu", "debian"
     action :add
   end
 
-
-when "redhat","centos","fedora", "amazon","scientific"
+elsif platform_family?("rhel")
+  # do RHEL things
 
   #Grab the major release for cent and rhel servers as this is what the repos use.
   releaseVersion = node['platform_version'].split('.').first
@@ -29,7 +48,7 @@ when "redhat","centos","fedora", "amazon","scientific"
   else
     signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc'
   end
-
+  
   yum_key "Rackspace-Monitoring" do
     url signingKey
     action :add
@@ -40,8 +59,8 @@ when "redhat","centos","fedora", "amazon","scientific"
     url "http://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{releaseVersion}-#{node['kernel']['machine']}"
     action :add
   end
-
 end
+
 
 begin
   databag_dir = node[:rackspace_cloudmonitoring]["credentials"]["databag_name"]
@@ -54,70 +73,38 @@ rescue Exception => e
   Chef::Log.error 'Failed to load rackspace cloud data bag: ' + e.to_s
 end
 
-
-#The first time this recipe runs on the node it is unable to pull token from the node attributes, unless they were put there by hand or in the data bag.
-#There's also no simple way to get data directly back from a provider.
-#So we're creating the auth_token with the LWRP, then using fog to pull it back out of the API. If you can find a better way to handle this, please rewrite it.
-if node[:rackspace_cloudmonitoring]['agent']['token'].nil?
-
-  if node[:rackspace_cloudmonitoring]['rackspace_username'] == "your_rackspace_username" or  node[:rackspace_cloudmonitoring]['rackspace_api_key'] == "your_rackspace_api_key"
-    raise RuntimeError, "No Rackspace credentials found"
-
-  #Create the token within the api, I'm using run_action to make sure everything happens in the proper order.
-  else
-    create_token = rackspace_cloudmonitoring_agent_token "#{node.hostname}" do
-      rackspace_username  node[:rackspace_cloudmonitoring]['rackspace_username']
-      rackspace_api_key   node[:rackspace_cloudmonitoring]['rackspace_api_key']
-      action :nothing
-    end
-
-    create_token.run_action(:create)
-
-    #Pull just the token itself into a variable named token
-    label = "#{node.hostname}"
-    monitoring = Fog::Rackspace::Monitoring.new(
-      :rackspace_api_key => node[:rackspace_cloudmonitoring]['rackspace_api_key'],
-      :rackspace_username => node[:rackspace_cloudmonitoring]['rackspace_username']
-    )
-    tokens = Hash[monitoring.agent_tokens.all.map  {|x| [x.label, x]}]
-    possible = tokens.select {|key, value| value.label === label}
-    possible = Hash[*possible.flatten(1)]
-
-    if !possible.empty? then
-      possible.values.first
-    else
-      nil
-    end
-
-    token = possible[label].token
-
-    if Chef::Config[:solo]
-      Chef::Log.warn("Under chef-solo, you must persist the agent token to " +
-                     "node[:rackspace_cloudmonitoring]['agent']['token'] or you will " +
-                     "regenerate the token every time. TOKEN: #{token}")
-    end
-
-    #Fire off a template run using the token pulled out of fog. This should only ever run on a new node, or if your node attributes get lost.
-    config_template = template "/etc/rackspace-monitoring-agent.cfg" do
-      source "rackspace-monitoring-agent.erb"
-      owner "root"
-      group "root"
-      mode 0600
-      variables(
-        :monitoring_id => "#{node.hostname}",
-        :monitoring_token =>  token
-      )
-      action :nothing
-    end
-
-    config_template.run_action(:create)
-
-  end
-
+# Call the agent_token LWRP to ensure we have a token in the API
+rackspace_cloudmonitoring_agent_token "#{node.hostname}" do
+  rackspace_username  node[:rackspace_cloudmonitoring]['rackspace_username']
+  rackspace_api_key   node[:rackspace_cloudmonitoring]['rackspace_api_key']
+  token               node[:rackspace_cloudmonitoring]['agent']['token']
+  action :create
 end
 
 
+# Hook into the cloud_monitoring module to get access to the CM_agent_token class
+# This is the easiest way to pull out the token and id generated by the LWRP
+class Chef::Recipe
+  include Opscode::Rackspace::Monitoring
+end
 
+my_token_obj = CM_agent_token.new(node[:rackspace_cloudmonitoring]['agent']['token'], "#{node.hostname}")
+my_token = my_token_obj.get_obj()
+
+# Generate the config template
+template "/etc/rackspace-monitoring-agent.cfg" do
+  source "rackspace-monitoring-agent.erb"
+  owner "root"
+  group "root"
+  mode 0600
+  variables(
+            # So the API calls it label, and the config calls it ID
+            # Clear as mud.
+            :monitoring_id => my_token.label,
+            :monitoring_token => my_token.token,
+            )
+  action :create
+end
 
 package "rackspace-monitoring-agent" do
   if node[:rackspace_cloudmonitoring]['agent']['version'] == 'latest'
@@ -128,19 +115,6 @@ package "rackspace-monitoring-agent" do
   end
 
   notifies :restart, "service[rackspace-monitoring-agent]"
-end
-
-unless node[:rackspace_cloudmonitoring]['agent']['token'].nil?
-  template "/etc/rackspace-monitoring-agent.cfg" do
-    source "rackspace-monitoring-agent.erb"
-    owner "root"
-    group "root"
-    mode 0600
-    variables(
-      :monitoring_id => node[:rackspace_cloudmonitoring]['agent']['id'],
-      :monitoring_token => node[:rackspace_cloudmonitoring]['agent']['token']
-    )
-  end
 end
 
 node[:rackspace_cloudmonitoring]['plugins'].each_pair do |source_cookbook, path|
