f65689ac352738f79d895d01af8124bbb1ddfb8d
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-16 17:25:54 -0600

    Added note on xml::ruby requirement

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Required to install fog
 include_recipe 'xml::ruby'
 
 chef_gem "fog" do
