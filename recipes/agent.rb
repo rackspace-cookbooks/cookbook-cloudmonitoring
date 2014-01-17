538445483d36ef2768ccf13d99f79b80e38e2021
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-17 17:48:03 -0600

    Rubocop fixes for agent.rb

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -25,7 +25,7 @@ if platform_family?('debian')
 
     if node['platform'] == 'ubuntu'
       uri "http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-#{node['platform_version']}-#{node['kernel']['machine']}"
-    elsif node['platform'] =='debian'
+    elsif node['platform'] == 'debian'
       uri "http://stable.packages.cloudmonitoring.rackspace.com/debian-#{node['lsb']['codename']}-#{node['kernel']['machine']}"
     end
 
@@ -39,25 +39,25 @@ elsif platform_family?('rhel')
   # do RHEL things
 
   # Grab the major release for cent and rhel servers as this is what the repos use.
-  releaseVersion = node['platform_version'].split('.').first
+  release_version = node['platform_version'].split('.').first
 
   # We need to figure out which signing key to use, cent5 and rhel5 have their own.
-  if (node['platform'] == 'centos') && (releaseVersion == '5')
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/centos-5.asc'
-  elsif (node['platform'] == 'redhat') && (releaseVersion == '5')
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-5.asc'
+  if (node['platform'] == 'centos') && (release_version == '5')
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/centos-5.asc'
+  elsif (node['platform'] == 'redhat') && (release_version == '5')
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-5.asc'
   else
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc'
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc'
   end
 
   rackspace_yum_key 'Rackspace-Monitoring' do
-    url signingKey
+    url signing_key
     action :add
   end
 
   rackspace_yum_repository 'cloud-monitoring' do
     description 'Rackspace Monitoring'
-    url "http://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{releaseVersion}-#{node['kernel']['machine']}"
+    url "http://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{release_version}-#{node['kernel']['machine']}"
     action :add
   end
 end
@@ -76,7 +76,6 @@ rackspace_cloudmonitoring_agent_token "#{node.hostname}" do
   token               node[:rackspace_cloudmonitoring][:agent][:token]
   action :create
 end
-
 
 my_token_obj = CM_agent_token.new(credentials, node[:rackspace_cloudmonitoring][:agent][:token], "#{node.hostname}")
 my_token = my_token_obj.get_obj
@@ -134,7 +133,7 @@ service 'rackspace-monitoring-agent' do
   )
 
   case node[:platform]
-    when 'ubuntu'
+  when 'ubuntu'
     if node[:platform_version].to_f >= 9.10
       provider Chef::Provider::Service::Upstart
     end
@@ -142,5 +141,4 @@ service 'rackspace-monitoring-agent' do
 
   action [:enable, :start]
   subscribes :restart, resources(template: '/etc/rackspace-monitoring-agent.cfg'), :delayed
-
 end
