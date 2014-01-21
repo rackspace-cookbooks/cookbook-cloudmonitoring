d5bb3d6925718b84073d00733a610ec749626768
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-21 13:45:02 -0600

    Comment error

--- libraries/cloud_monitoring.rb
+++ libraries/cloud_monitoring.rb
@@ -241,7 +241,7 @@ module Opscode
         # RETURN VALUE: None
         # Opens @cm class variable
         def initialize(credentials)
-          # This class intentionally uses a class method to share Fog connections across class instances
+          # This class intentionally uses a class variable to share Fog connections across class instances
           # The class variable is guarded by use of the CMCache class which ensures proper connections are utilized
           #    across different class instances.
           # Basically we're in a corner case where class variables are called for.
@@ -387,7 +387,7 @@ module Opscode
         # POST: None
         # RETURN VALUE: None
         def initialize(credentials, my_chef_label)
-          # This class intentionally uses a class method to share entity IDs across class instances
+          # This class intentionally uses a class variable to share entity IDs across class instances
           # The class variable is guarded by use of the CMCache class which ensures IDs are utilized
           #    properly across different class instances.
           # Basically we're in a corner case where class variables are called for.
@@ -554,7 +554,7 @@ module Opscode
         # POST: None
         # RETURN VALUE: None
         def initialize(credentials, entity_chef_label, my_target_name, my_debug_name, my_label)
-          # This class intentionally uses a class method to share object IDs across class instances
+          # This class intentionally uses a class variable to share object IDs across class instances
           # The class variable is guarded by use of the CMCache class which ensures IDs are utilized
           #    properly across different class instances.
           # Basically we're in a corner case where class variables are called for.
