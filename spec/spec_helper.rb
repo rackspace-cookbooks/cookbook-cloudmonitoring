e4b66cc699841638c5c5caa972f8d639ece3ba78
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-31 17:01:06 -0600

    Turn down ChefSpec debug level

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,5 +27,5 @@ require_relative 'supported_platforms.rb'
 RSpec.configure do |c|
   c.filter_run(focus: true)
   c.run_all_when_everything_filtered = true
-  c.log_level = :debug
+  c.log_level = :warn
 end
