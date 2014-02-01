7dd61e49a9e5683e11e9765b8d323867acf3321f
Tom Noonan II <thomas.noonan@rackspace.com>
2014-01-31 20:58:06 -0600

    Added template content checks to the agent_explicit_args test

--- test/integration/agent_API/serverspec/agent_spec.rb
+++ test/integration/agent_API/serverspec/agent_spec.rb
@@ -7,11 +7,6 @@ require 'spec_helper'
 # Currently no check if the agent registers and is connecting to the API, but in
 #   its current form we can feed it dummy data and not worry about account pollution.
 describe 'Server With CloudMonitoring Agent' do
-  describe file('/etc/rackspace-monitoring-agent.cfg') do
-    it { should contain 'rackspacerules' } # test ID
-    it { should contain 'kittenmittens' }  # test token
-  end  
-
   it 'should have the agent running' do
     expect(service 'rackspace-monitoring-agent').to be_running
     expect(service 'rackspace-monitoring-agent').to be_enabled
