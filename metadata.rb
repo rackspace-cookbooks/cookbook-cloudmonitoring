b246b14adce350945bce6a57c75da3455258ed78
Tom Noonan II <thomas.noonan@rackspace.com>
2014-04-15 08:31:29 -0500

    Feature add: bump minor

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures Rackspace Cloud Monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '3.0.0'
+version          '3.1.0'
 
 depends 'rackspace_apt', '~> 3.0'
 depends 'rackspace_yum', '~> 4.0'
