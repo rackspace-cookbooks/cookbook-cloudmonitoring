a5273aabbba6f03b4b59934700e843cd2a344ace
Tom Noonan II <thomas.noonan@rackspace.com>
2014-05-08 15:35:00 -0500

    Disable ClassLength cop

--- libraries/CMCredentials.rb
+++ libraries/CMCredentials.rb
@@ -22,7 +22,11 @@ module Opscode
   module Rackspace
     module Monitoring
       # CMCredentials: Class for handling the various credential sources
-      class CMCredentials
+      #
+      # As of 5/8/14 this class is two lines too long, mostly from the attribute map.
+      # While cutting 3 lines is doable, it would just pop back when a new attribute is defined
+      #  and would not improve overall code quality.  Disable the ClassLength cop here.
+      class CMCredentials # rubocop:disable ClassLength
         def initialize(my_node, my_resource)
           @node = my_node
           @resource = my_resource
