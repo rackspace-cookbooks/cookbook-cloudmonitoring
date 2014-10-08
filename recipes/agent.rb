ac64c1834bc8a7fac09b360244c4724a761a320c
Alberto.klocker <alberto.klocker@bluereef.com.au>
2014-10-09 09:55:42 +1100

    Trying a loop for token creation so it never fails, this is terrible

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -69,25 +69,28 @@ if node['cloud_monitoring']['agent']['token'].nil?
       action :nothing
     end
 
-    create_token.run_action(:create)
+    token = nil
+    while token == nil do
+      create_token.run_action(:create)
 
-    #Pull just the token itself into a variable named token
-    label = "#{node.hostname}"
-    monitoring = Fog::Rackspace::Monitoring.new(
-      :rackspace_api_key => node['cloud_monitoring']['rackspace_api_key'],
-      :rackspace_username => node['cloud_monitoring']['rackspace_username']
-    )
-    tokens = Hash[monitoring.agent_tokens.all.map  {|x| [x.label, x]}]
-    possible = tokens.select {|key, value| value.label === label}
-    possible = Hash[*possible.flatten(1)]
+      #Pull just the token itself into a variable named token
+      label = "#{node.hostname}"
+      monitoring = Fog::Rackspace::Monitoring.new(
+	:rackspace_api_key => node['cloud_monitoring']['rackspace_api_key'],
+	:rackspace_username => node['cloud_monitoring']['rackspace_username']
+      )
+      tokens = Hash[monitoring.agent_tokens.all.map  {|x| [x.label, x]}]
+      possible = tokens.select {|key, value| value.label === label}
+      possible = Hash[*possible.flatten(1)]
 
-    if !possible.empty? then
-      possible.values.first 
-    else
-      nil
+      if !possible.empty? then
+	possible.values.first 
+      else
+	nil
+      end
+      
+      token = possible[label].token
     end
-    
-    token = possible[label].token
 
     if Chef::Config[:solo]
       Chef::Log.warn("Under chef-solo, you must persist the agent token to " +
