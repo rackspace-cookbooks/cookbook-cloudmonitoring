7837176622960b4eef5a437f58e1856b275bfdb4
dann7387 <danny.cheung@rackspace.com>
2015-01-06 02:41:02 +0000

    Re-configure the logic to be more idempotent.

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -1,5 +1,18 @@
+include_recipe 'xml::ruby'
+
+chef_gem "fog" do
+  version ">= #{node['cloud_monitoring']['fog_version']}"
+  action :install
+end
+
 include_recipe 'cloud_monitoring::agent_repo.rb'
 
+# Get the agent id
+if node['cloud_monitoring']['agent']['id'].nil?
+  node.set['cloud_monitoring']['agent']['id'] = node['hostname']
+end
+
+# Try to retireve agent token from the data bag
 begin
   databag_dir = node["cloud_monitoring"]["credentials"]["databag_name"]
   databag_filename = node["cloud_monitoring"]["credentials"]["databag_item"]
@@ -11,48 +24,20 @@ rescue Exception => e
   Chef::Log.error 'Failed to load rackspace cloud data bag: ' + e.to_s
 end
 
-
-#The first time this recipe runs on the node it is unable to pull token from the node attributes, unless they were put there by hand or in the data bag.
-#There's also no simple way to get data directly back from a provider.
-#So we're creating the auth_token with the LWRP, then using fog to pull it back out of the API. If you can find a better way to handle this, please rewrite it.
 if node['cloud_monitoring']['agent']['token'].nil?
+  get_agent_token
+end
 
-  if node['cloud_monitoring']['rackspace_username'] == "your_rackspace_username" or  node['cloud_monitoring']['rackspace_api_key'] == "your_rackspace_api_key"
-    raise RuntimeError, "No Rackspace credentials found"
-
-  #Create the token within the api, I'm using run_action to make sure everything happens in the proper order.
-  else
+# If unable to retieve the agent token via API, create a new one
+if node['cloud_monitoring']['agent']['token'].nil?
     create_token = cloud_monitoring_agent_token "#{node.hostname}" do
       rackspace_username  node['cloud_monitoring']['rackspace_username']
       rackspace_api_key   node['cloud_monitoring']['rackspace_api_key']
-      action :nothing
+      action :create
     end
-
-    create_token.run_action(:create)
 
     retrieve_agent_token
-
-    #Fire off a template run using the token pulled out of fog. This should only ever run on a new node, or if your node attributes get lost.
-    config_template = template "/etc/rackspace-monitoring-agent.cfg" do
-      source "rackspace-monitoring-agent.erb"
-      owner "root"
-      group "root"
-      mode 0600
-      variables(
-        :monitoring_id => "#{node.hostname}",
-        :monitoring_token =>  token
-      )
-      action :nothing
-    end
-
-    config_template.run_action(:create)
-
-  end
-
 end
-
-
-
 
 package "rackspace-monitoring-agent" do
   if node['cloud_monitoring']['agent']['version'] == 'latest'
