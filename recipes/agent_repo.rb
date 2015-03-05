76203f0ac15509144b047687b27b6a5c98c91263
dann7387 <danny.cheung@rackspace.com>
2015-03-05 02:39:48 +0000

    Fix rubocop errors.

--- recipes/agent_repo.rb
+++ recipes/agent_repo.rb
@@ -17,23 +17,23 @@ when 'ubuntu', 'debian'
 when 'redhat', 'centos', 'fedora', 'amazon', 'scientific'
 
   # Grab the major release for cent and rhel servers as this is what the repos use.
-  releaseVersion = node['platform_version'].split('.').first
+  release_version = node['platform_version'].split('.').first
 
   # We need to figure out which signing key to use, cent5 and rhel5 have their own.
-  if (node['platform'] == 'centos') && (releaseVersion == '5')
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/centos-5.asc'
-  elsif (node['platform'] == 'redhat') && (releaseVersion == '5')
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-5.asc'
-  elsif (node['platform'] == 'redhat') && (releaseVersion == '6')
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-6.asc'
+  if (node['platform'] == 'centos') && (release_version == '5')
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/centos-5.asc'
+  elsif (node['platform'] == 'redhat') && (release_version == '5')
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-5.asc'
+  elsif (node['platform'] == 'redhat') && (release_version == '6')
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/redhat-6.asc'
   else
-    signingKey = 'https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc'
+    signing_key = 'https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc'
   end
 
   yum_repository 'cloud-monitoring' do
     description 'Rackspace Monitoring'
-    url "http://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{releaseVersion}-#{node['kernel']['machine']}"
-    gpgkey signingKey
+    url "http://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{release_version}-#{node['kernel']['machine']}"
+    gpgkey signing_key
     action :add
   end
 end
