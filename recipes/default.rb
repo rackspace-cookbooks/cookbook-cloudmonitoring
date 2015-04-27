ddf71f19860a26afb800939a9e9e3e38d08122d1
Michael Burns <michael.burns@rackspace.com>
2015-04-27 03:04:41 -0700

    Monitoring agent installtion fails occasionally due to outdated setuptools

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,7 +23,18 @@ chef_gem 'fog' do
   action :install
 end
 
+chef_gem 'httparty' do
+  version '>=0.13.1'
+  action :install
+end
+
+chef_gem 'json' do
+  action :install
+end
+
 require 'fog'
+require 'httparty'
+require 'json'
 
 begin
   # Access the Rackspace Cloud encrypted data_bag
